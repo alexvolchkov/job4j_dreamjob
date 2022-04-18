836a8f73c65eb100b5be378c0a86dcd61abcc69e
alexvolchkov <volkaa@mail.ru>
2022-04-18 19:58:41 +0300

    3. PostController.createPost. Создание вакансии. [#504849]

--- src/main/java/ru/job4j/dreamjob/model/Post.java
+++ src/main/java/ru/job4j/dreamjob/model/Post.java
@@ -9,6 +9,9 @@ public class Post {
     private String description;
     private LocalDate created;
 
+    public Post() {
+    }
+
     public Post(int id, String name) {
         this.id = id;
         this.name = name;
