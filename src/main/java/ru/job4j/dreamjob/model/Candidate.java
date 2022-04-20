ed7c618791fc1fe43f7d731d542b0f10c0fba267
alexvolchkov <volkaa@mail.ru>
2022-04-20 13:33:57 +0300

    5. Создания и редактирования кандидатов. [#504858]

--- src/main/java/ru/job4j/dreamjob/model/Candidate.java
+++ src/main/java/ru/job4j/dreamjob/model/Candidate.java
@@ -9,6 +9,9 @@ public class Candidate {
     private String desc;
     private LocalDate created;
 
+    public Candidate() {
+    }
+
     public Candidate(int id, String name) {
         this.id = id;
         this.name = name;
--- src/main/java/ru/job4j/dreamjob/store/CandidateStore.java
+++ src/main/java/ru/job4j/dreamjob/store/CandidateStore.java
@@ -6,10 +6,12 @@ import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class CandidateStore {
     private static final CandidateStore INST = new CandidateStore();
     private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+    private final AtomicInteger counter = new AtomicInteger(4);
 
     public CandidateStore() {
         candidates.put(1, new Candidate(1, "Alex", "Junior Java Job", LocalDate.now()));
@@ -23,5 +25,18 @@ public class CandidateStore {
 
     public Collection<Candidate> findAll() {
         return candidates.values();
+    }
+
+    public Candidate add(Candidate candidate) {
+        candidate.setId(counter.getAndIncrement());
+        return candidates.put(candidate.getId(), candidate);
+    }
+
+    public Candidate findById(int id) {
+        return candidates.get(id);
+    }
+
+    public boolean update(Candidate candidate) {
+        return candidates.replace(candidate.getId(), candidate) != null;
     }
 }
